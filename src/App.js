4f9b560f8f7e1ca14198da02c8267404d8e04c7a
alppacat <lorena.pratts@gmail.com>
2018-12-03 19:33:12 -0600

    Arregla conflictos de login y rutas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-// import { Redirect, BrowserRouter, Switch, Route } from "react-router-dom";
-import './App.css';
-import Login from './components/Login/Login';
+import React, { Component } from "react";
+import { Redirect, BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./Components/Login/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>prueba</p>
-        <Login />
-      </div>
-      // <BrowserRouter>
-      //   <div className="App">
-      //     <Switch>
-      //       <p>prueba</p>
-      //       <Route path="/" component={Login} exact />
-      //       <Route path="registro" component={Registro} />
-      //       <Route path="home" component={Home} />
-      //     </Switch>
-      //   </div>
-      // </BrowserRouter>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/" component={Login} exact />
+            {/* <Route path="registro" component={Registro} />
+            <Route path="home" component={Home} /> */}
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
